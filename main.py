c27c9639470ce6a0cf28f863fc636143952c46d3
Martí Caixal Joaniquet <mcaixal1999@gmail.com>
2021-11-04 19:53:41 +0100

    changing attributes from continue to discrete

--- main.py
+++ main.py
@@ -12,14 +12,22 @@ def main():
     for name in df.columns:
         print(len(pd.unique(df[name])), name, " unique values: " )
 
-    sns.boxplot(data=df)
-    plt.show()
-    df.hist(figsize=(8, 8))
-    plt.show()
+    # sns.boxplot(data=df)
+    # plt.show()
+    # df.hist(figsize=(8, 8))
+    # plt.show()
 
+    df['age_qcut'] = pd.qcut(df['age'], 10)
+    df['age_cut'] = pd.cut(df['age'], 10)
+    df['trestbps_cut'] = pd.cut(df['trestbps'], 10)
+    df['chol_cut'] = pd.cut(df['chol'], 10)
+    df['chol_cut'] = pd.cut(df['chol'], 10)
+    df['thalach_cut'] = pd.cut(df['thalach'], 10)
+    df['oldpeak_cut'] = pd.cut(df['oldpeak'], 10)
 
-
-
+    print(df[['age', 'age_qcut', 'age_cut']].head(10))
+    print(df[['age_qcut']].value_counts())
+    print(df[['age_cut']].value_counts())
 
 
 if __name__ == "__main__":
