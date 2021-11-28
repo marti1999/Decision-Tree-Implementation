95ce32c76cc24c81434af6712bbeebe9ee3ece0c
marti1999 <mcaixal1999@gmail.com>
2021-11-28 17:13:03 +0100

    fixing bug where dataframe would be overwriten inside functions

--- main.py
+++ main.py
@@ -42,20 +42,20 @@ def main():
     # test1Model(copy.deepcopy(df))
 
     # PER PROVAR EL NOSTRE CROSS VALIDATION I DIFERENTS HEURÍSTIQUES
-    # testCrossvalidationHeuristics(df, ['id3', 'c45', 'gini'], intervals=[4, 6])
+    # testCrossvalidationHeuristics(copy.deepcopy(df), ['id3', 'c45', 'gini'], intervals=[4, 6])
 
     # PER PROVAR EL NOSTRE CROSS VALIDATION I PROBABILISTIC APPROACH
-    # testCrossvalidationProbabilisticApproach(df, [False, True], intervals=[4, 6], heuristic='gini')
+    # testCrossvalidationProbabilisticApproach(copy.deepcopy(df), [False, True], intervals=[4, 6], heuristic='gini')
 
     # PER PROVAR EL TWO-WAY SPLIT
-    # testCrossvalidationTwoWaySplit(df, intervals=[5, 10, 20,30, 40, 50, 60, 70, 100, 200, 500], heuristic='gini')
+    # testCrossvalidationTwoWaySplit(copy.deepcopy(df), intervals=[5, 10, 20,30, 40, 50, 60, 70, 100, 200, 500], heuristic='gini')
 
     # PER COMPARAR TEMPS D'EXECUCIÓ INTERVALS VS 2-WAY SPLIT
-    # testExecutionTime2waysplitVSintervals(df, test_size=0.5, proba=True)
+    # testExecutionTime2waysplitVSintervals(copy.deepcopy(df), test_size=0.5, proba=True)
 
     # IMPLEMENTACIONS AMB SKLEARN, PER FER COMPARACIONS
-    # crossValidationSklearn(df)
-    # compareWithSklearn(df)
+    # crossValidationSklearn(copy.deepcopy(df))
+    # compareWithSklearn(copy.deepcopy(df))
 
 
 if __name__ == "__main__":
